53855a3a39fb9c926ef6b8ba10ce4750a87f4bf4
Stephen <clarksj4@student.op.ac.nz>
2017-06-01 14:41:15 +1200

    Rename Pawn interface to ITurnBased

--- TurnSystem/ITurnBased.cs
+++ TurnSystem/ITurnBased.cs
@@ -3,22 +3,22 @@
 namespace TurnBased
 {
     /// <summary>
-    /// Interface for object that operates in a turn based manner
+    /// Turn order member object that operates in a turn based manner. 
     /// </summary>
-    public interface IPawn<T> where T : IComparable<T>
+    public interface ITurnBased<T> where T : IComparable<T>
     {
         /// <summary>
-        /// The pawn's priority in the turn order
+        /// The object's priority in the turn order
         /// </summary>
         T Priority { get; }
 
         /// <summary>
-        /// The pawn's turn is starting
+        /// The object's turn is starting
         /// </summary>
         void TurnStart();
 
         /// <summary>
-        /// The pawn's turn is ending
+        /// The object's turn is ending
         /// </summary>
         void TurnEnd();
 
